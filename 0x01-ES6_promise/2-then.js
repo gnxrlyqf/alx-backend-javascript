404960390ea5020fcd6d5595038dfe949e89cb1b
gnxrlyqf <gnarlydestructor@outlook.com>
2024-06-27 01:29:35 +0100

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,7 @@ export default function handleResponseFromAPI(promise) {
     .then(() => ({
       status: 200,
       body: 'success',
-    })).catch(() => new Error())
+    }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
