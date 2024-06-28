ae601fa0e562b924c069e5d1700ce3a29c0ee77e
gnxrlyqf <gnarlydestructor@outlook.com>
2024-06-28 22:13:08 +0100

    2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -29,7 +29,7 @@ export default class HolbertonCourse {
   get students() { return this._students; }
 
   set students(students) {
-    if (students instanceof Array) {
+    if (students instanceof Array && students.every((student) => typeof student === 'string')) {
       this._students = students;
     } else {
       throw new TypeError('Students must be an array');
