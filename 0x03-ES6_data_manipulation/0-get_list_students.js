130f24c0379b2245877bb2154a6f6ba99d83cd4d
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-03 00:26:18 +0100

    0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
-	return [
-	  {
+  return [
+    {
       id: 1,
       firstName: 'Guillaume',
-      location: 'San Francisco'
+      location: 'San Francisco',
     },
-	  {
+    {
       id: 2,
       firstName: 'James',
-      location: 'Columbia'
+      location: 'Columbia',
     },
-	  {
+    {
       id: 5,
       firstName: 'Serena',
-      location: 'San Francisco'
+      location: 'San Francisco',
     },
-	];
+  ];
 }
