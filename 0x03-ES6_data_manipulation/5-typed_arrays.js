e2c37dbfa97ead05259768d046aa79d6c89a71de
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-03 01:19:46 +0100

    5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(
+    new ArrayBuffer(length),
+    0,
+    length,
+  );
+  buf.setUInt8(position, value);
+  return buf;
+}
